2642f8a350156876a9567b2f550b0b806ab9c4c1
Chris Smith <chris@cir.ca>
2013-05-07 23:16:24 -0700

    api documentation update

--- arrow/api.py
+++ arrow/api.py
@@ -7,7 +7,67 @@ from dateutil import tz as dateutil_tz
 def get(*args, **kwargs):
     '''Returns an :class:`Arrow <arrow.Arrow>` object based on flexible inputs.
 
+    Usage::
 
+        >>> import arrow
+
+    **No** inputs to get **current UTC time**::
+
+        >>> arrow.get()
+        <Arrow [2013-05-08T05:51:43.316458+00:00]>
+
+    **One str**, **float**, or **int**, convertible to a floating-point timestamp, to get that timestamp in UTC::
+
+        >>> arrow.get(1367992474.293378)
+        <Arrow [2013-05-08T05:54:34.293378+00:00]>
+        >>>
+        >>> arrow.get(1367992474)
+        <Arrow [2013-05-08T05:54:34+00:00]>
+        >>>
+        >>> arrow.get('1367992474.293378')
+        <Arrow [2013-05-08T05:54:34.293378+00:00]>
+        >>>
+        >>> arrow.get('1367992474')
+        <Arrow [2013-05-08T05:54:34+00:00]>
+
+    **One str**, convertible to a timezone, or **tzinfo**, to get the current time in that timezone::
+
+        >>> arrow.get('local')
+        <Arrow [2013-05-07T22:57:11.793643-07:00]>
+        >>>
+        >>> arrow.get('US/Pacific')
+        <Arrow [2013-05-07T22:57:15.609802-07:00]>
+        >>>
+        >>> arrow.get('-07:00')
+        <Arrow [2013-05-07T22:57:22.777398-07:00]>
+        >>>
+        >>> arrow.get(tz.tzlocal())
+        <Arrow [2013-05-07T22:57:28.484717-07:00]>
+
+    **One** naive **datetime**, to get that datetime in UTC::
+
+        >>> arrow.get(datetime(2013, 5, 5))
+        <Arrow [2013-05-05T00:00:00+00:00]>
+
+    **One** aware **datetime**, to get that datetime::
+
+        >>> arrow.get(datetime(2013, 5, 5, tzinfo=tz.tzlocal()))
+        <Arrow [2013-05-05T00:00:00-07:00]>
+
+    **Two** arguments, a naive or aware datetime, and a timezone expression (as above)::
+
+        >>> arrow.get(datetime(2013, 5, 5), 'US/Pacific')
+        <Arrow [2013-05-05T00:00:00-07:00]>
+
+    **Two** arguments, both strings, to parse the first according to the format of the second::
+
+        >>> arrow.get('2013-05-05 12:30:45', 'YYYY-MM-DD HH:mm:ss')
+        <Arrow [2013-05-05T12:30:45+00:00]>
+
+    **Three or more** arguments, as for the constructor of a **datetime**::
+
+        >>> arrow.get(2013, 5, 5, 12, 30, 45)
+        <Arrow [2013-05-05T12:30:45+00:00]>
     '''
 
     arg_count = len(args)
