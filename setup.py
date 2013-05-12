704d385569bda97640167998e8212a056b549846
Chris Smith <chris@cir.ca>
2013-05-11 20:21:13 -0700

    metadata update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='arrow',
-    version='0.2.1',
+    version='0.3.0',
     description='Better dates and times for Python',
     url='http://crsmithdev.com/arrow',
     author='Chris Smith',
