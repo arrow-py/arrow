df9f6232343a14db9d3b6ee2f9c70340c3749e67
Ankur Dedania <AbsoluteMSTR@gmail.com>
2013-05-23 15:34:50 -0500

    fix issue in fbb472c and 7e1a4f9

--- arrow/const.py
+++ arrow/const.py
@@ -6,21 +6,20 @@ import calendar
 MONTH_NAME_MAP = dict(map(lambda i: (i[0] + 1, i[1]),
                           enumerate(calendar.month_name[1:])))
 
-MONTH_VALUE_NAME_MAP = dict(map(lambda i: (i[0] + 1, i[1]),
+MONTH_VALUE_NAME_MAP = dict(map(lambda i: (i[1], i[0] + 1),
                                 enumerate(calendar.month_name[1:])))
 
 MONTH_ABBR_MAP = dict(map(lambda i: (i[0] + 1, i[1]),
                           enumerate(calendar.month_abbr[1:])))
 
-MONTH_VALUE_ABBR_MAP = dict(map(lambda i: (i[0] + 1, i[1]),
-                          enumerate(calendar.month_abbr[1:])))
+MONTH_VALUE_ABBR_MAP = dict(map(lambda i: (i[1], i[0] + 1),
+                                enumerate(calendar.month_abbr[1:])))
 
 DAY_NAME_MAP = dict(map(lambda i: (i[0] + 1, i[1]),
                         enumerate(calendar.day_name[1:])))
 
 DAY_ABBR_MAP = dict(map(lambda i: (i[0] + 1, i[1]),
                         enumerate(calendar.day_abbr[1:])))
-
 
 __all__ = [
     'MONTH_NAME_MAP',
