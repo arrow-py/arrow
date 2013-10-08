29168a7f254a2e5b7db7bb10aa501ba7cf9d9e61
Chris Smith <crsmithdev@gmail.com>
2013-10-08 08:16:50 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='arrow',
-    version='0.3.5',
+    version='0.4.0',
     description='Better dates and times for Python',
     url='http://crsmithdev.com/arrow',
     author='Chris Smith',
