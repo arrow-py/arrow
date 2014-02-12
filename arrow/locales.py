e66bcdadc6631ab95cea72def08e83bdfb26e6d9
Hólmar Sigmundsson <holmars@gmail.com>
2014-02-12 21:53:28 +0100

    Add Danish locale

--- arrow/locales.py
+++ arrow/locales.py
@@ -968,6 +968,37 @@ class IcelandicLocale(Locale):
     day_abbreviations = ['', 'mán', 'þri', 'mið', 'fim', 'fös', 'lau', 'sun']
 
 
+class DanishLocale(Locale):
+
+    names = ['da', 'da_dk']
+
+    past = 'for {0} siden'
+    future = 'efter {0}'
+
+    timeframes = {
+        'now':     'lige nu',
+        'seconds': 'et par sekunder',
+        'minute':  'et minut',
+        'minutes': '{0} minutter',
+        'hour':    'en time',
+        'hours':   '{0} timer',
+        'day':     'en dag',
+        'days':    '{0} dage',
+        'month':   'en måned',
+        'months':  '{0} måneder',
+        'year':    'et år',
+        'years':   '{0} år',
+    }
+
+    month_names = ['', 'januar', 'februar', 'marts', 'april', 'maj', 'juni',
+        'juli', 'august', 'september', 'oktober', 'november', 'december']
+    month_abbreviations = ['', 'jan', 'feb', 'mar', 'apr', 'maj', 'jun',
+        'jul', 'aug', 'sep', 'okt', 'nov', 'dec']
+
+    day_names = ['', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag',
+        'lørdag', 'søndag']
+    day_abbreviations = ['', 'man', 'tir', 'ons', 'tor', 'fre', 'lør', 'søn']
+
 def _map_locales():
 
     locales = {}
