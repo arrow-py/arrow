6b86d838ffb6815658a406942cf720e7877985ab
Andrew Elkins <github2@andrewelkins.com>
2015-01-22 10:43:03 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='arrow',
-    version='0.4.4',
+    version='0.4.5',
     description='Better dates and times for Python',
     url='http://crsmithdev.com/arrow',
     author='Chris Smith',
