78ebd731dd2df10d62df9aa2adef8f85a42ef020
Evan Gui <evan176@hotmail.com>
2016-02-17 18:25:45 +0800

    Add relativedelta to __sub__

--- arrow/arrow.py
+++ arrow/arrow.py
@@ -678,7 +678,7 @@ class Arrow(object):
 
     def __sub__(self, other):
 
-        if isinstance(other, timedelta):
+        if isinstance(other, (timedelta, relativedelta)):
             return self.fromdatetime(self._datetime - other,
                                      self._datetime.tzinfo)
 
