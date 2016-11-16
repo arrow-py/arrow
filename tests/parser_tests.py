bc03b0b5337642c6766cda4f483074e0ecc88455
Buck Evan <buck@yelp.com>
2016-11-16 09:52:52 -0800

    fix parsing of iso-8601 dates as emitted by GNU date -Ins

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -188,17 +188,34 @@ class DateTimeParserParseTests(Chai):
         assertEqual(self.parser.parse('2013-01-01 12:30:45.987654', 'YYYY-MM-DD HH:mm:ss.SSSSSS'), expected)
         assertEqual(self.parser.parse_iso('2013-01-01 12:30:45.987654'), expected)
 
-        expected = datetime(2013, 1, 1, 12, 30, 45, 987654)
-        assertEqual(self.parser.parse('2013-01-01 12:30:45.9876543', 'YYYY-MM-DD HH:mm:ss.SSSSSS'), expected)
-        assertEqual(self.parser.parse_iso('2013-01-01 12:30:45.9876543'), expected)
+    def test_parse_subsecond_rounding(self):
+        """currently, we've decided there's no rounding"""
+        format = 'YYYY-MM-DD HH:mm:ss.S'
 
-        expected = datetime(2013, 1, 1, 12, 30, 45, 987654)
-        assertEqual(self.parser.parse('2013-01-01 12:30:45.98765432', 'YYYY-MM-DD HH:mm:ss.SSSSSS'), expected)
-        assertEqual(self.parser.parse_iso('2013-01-01 12:30:45.98765432'), expected)
+        # round up
+        string = '2013-01-01 12:30:45.9876539'
+        expected = datetime(2013, 1, 1, 12, 30, 45, 987653)
+        assertEqual(self.parser.parse(string, format), expected)
+        assertEqual(self.parser.parse_iso(string), expected)
 
+        # round down
+        string = '2013-01-01 12:30:45.98765432'
         expected = datetime(2013, 1, 1, 12, 30, 45, 987654)
-        assertEqual(self.parser.parse('2013-01-01 12:30:45.987654321', 'YYYY-MM-DD HH:mm:ss.SSSSSS'), expected)
-        assertEqual(self.parser.parse_iso('2013-01-01 12:30:45.987654321'), expected)
+        assertEqual(self.parser.parse(string, format), expected)
+        #import pudb; pudb.set_trace()
+        assertEqual(self.parser.parse_iso(string), expected)
+
+        # round half-up
+        string = '2013-01-01 12:30:45.987653521'
+        expected = datetime(2013, 1, 1, 12, 30, 45, 987653)
+        assertEqual(self.parser.parse(string, format), expected)
+        assertEqual(self.parser.parse_iso(string), expected)
+
+        # round half-down
+        string = '2013-01-01 12:30:45.9876545210'
+        expected = datetime(2013, 1, 1, 12, 30, 45, 987654)
+        assertEqual(self.parser.parse(string, format), expected)
+        assertEqual(self.parser.parse_iso(string), expected)
 
     def test_map_lookup_keyerror(self):
 
@@ -398,6 +415,21 @@ class DateTimeParserISOTests(Chai):
             datetime(2013, 2, 3, 4, 5, 6, 789120)
         )
 
+        # ISO 8601:2004(E), ISO, 2004-12-01, 4.2.2.4 ... the decimal fraction
+        # shall be divided from the integer part by the decimal sign specified
+        # in ISO 31-0, i.e. the comma [,] or full stop [.]. Of these, the comma
+        # is the preferred sign.
+        assertEqual(
+            self.parser.parse_iso('2013-02-03T04:05:06,789123678'),
+            datetime(2013, 2, 3, 4, 5, 6, 789123)
+        )
+
+        # there is no limit on the number of decimal places
+        assertEqual(
+            self.parser.parse_iso('2013-02-03T04:05:06.789123678'),
+            datetime(2013, 2, 3, 4, 5, 6, 789123)
+        )
+
     def test_YYYY_MM_DDTHH_mm_ss_SZ(self):
 
         assertEqual(
@@ -429,6 +461,28 @@ class DateTimeParserISOTests(Chai):
         assertEqual(
             self.parser.parse_iso('2013-02-03T04:05:06.78912Z'),
             datetime(2013, 2, 3, 4, 5, 6, 789120)
+        )
+
+    def test_gnu_date(self):
+        """
+        regression tests for parsing output from GNU date(1)
+        """
+        # date -Ins
+        assertEqual(
+            self.parser.parse_iso('2016-11-16T09:46:30,895636557-0800'),
+            datetime(
+                2016, 11, 16, 9, 46, 30, 895636,
+                tzinfo=tz.tzoffset(None, -3600 * 8),
+            )
+        )
+
+        # date --rfc-3339=ns
+        assertEqual(
+            self.parser.parse_iso('2016-11-16 09:51:14.682141526-08:00'),
+            datetime(
+                2016, 11, 16, 9, 51, 14, 682141,
+                tzinfo=tz.tzoffset(None, -3600 * 8),
+            )
         )
 
     def test_isoformat(self):
