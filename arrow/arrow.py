a879ec0ebd54bfaf7655830b4f95fd7bd5e72ad5
Guillaume Mösching <guillaume.mosching@gmail.com>
2016-12-01 17:37:47 -0500

    Main functionnalities

--- arrow/arrow.py
+++ arrow/arrow.py
@@ -10,6 +10,7 @@ from __future__ import absolute_import
 from datetime import datetime, timedelta, tzinfo
 from dateutil import tz as dateutil_tz
 from dateutil.relativedelta import relativedelta
+from math import trunc
 import calendar
 import sys
 import warnings
@@ -641,14 +642,14 @@ class Arrow(object):
         return formatter.DateTimeFormatter(locale).format(self._datetime, fmt)
 
 
-    def humanize(self, other=None, locale='en_us', only_distance=False):
+    def humanize(self, other=None, locale='en_us', only_distance=False, granularity='auto'):
         ''' Returns a localized, humanized representation of a relative difference in time.
 
         :param other: (optional) an :class:`Arrow <arrow.arrow.Arrow>` or ``datetime`` object.
             Defaults to now in the current :class:`Arrow <arrow.arrow.Arrow>` object's timezone.
         :param locale: (optional) a ``str`` specifying a locale.  Defaults to 'en_us'.
         :param only_distance: (optional) returns only time difference eg: "11 seconds" without "in" or "ago" part.
-
+        :param granularity: (optinal) defines the precision of the output. Set it to strings 'second', 'minute', 'hour', 'day', 'month' or 'year'.
         Usage::
 
             >>> earlier = arrow.utcnow().replace(hours=-2)
@@ -683,48 +684,71 @@ class Arrow(object):
         sign = -1 if delta < 0 else 1
         diff = abs(delta)
         delta = diff
+        
+        if granularity=='auto':
+            if diff < 10:
+                return locale.describe('now', only_distance=only_distance)
+    
+            if diff < 45:
+                seconds = sign * delta
+                return locale.describe('seconds', seconds, only_distance=only_distance)
+    
+            elif diff < 90:
+                return locale.describe('minute', sign, only_distance=only_distance)
+            elif diff < 2700:
+                minutes = sign * int(max(delta / 60, 2))
+                return locale.describe('minutes', minutes, only_distance=only_distance)
+    
+            elif diff < 5400:
+                return locale.describe('hour', sign, only_distance=only_distance)
+            elif diff < 79200:
+                hours = sign * int(max(delta / 3600, 2))
+                return locale.describe('hours', hours, only_distance=only_distance)
+    
+            elif diff < 129600:
+                return locale.describe('day', sign, only_distance=only_distance)
+            elif diff < 2160000:
+                days = sign * int(max(delta / 86400, 2))
+                return locale.describe('days', days, only_distance=only_distance)
+    
+            elif diff < 3888000:
+                return locale.describe('month', sign, only_distance=only_distance)
+            elif diff < 29808000:
+                self_months = self._datetime.year * 12 + self._datetime.month
+                other_months = dt.year * 12 + dt.month
+    
+                months = sign * int(max(abs(other_months - self_months), 2))
+    
+                return locale.describe('months', months, only_distance=only_distance)
+    
+            elif diff < 47260800:
+                return locale.describe('year', sign, only_distance=only_distance)
+            else:
+                years = sign * int(max(delta / 31536000, 2))
+                return locale.describe('years', years, only_distance=only_distance)
 
-        if diff < 10:
-            return locale.describe('now', only_distance=only_distance)
-
-        if diff < 45:
-            return locale.describe('seconds', sign, only_distance=only_distance)
-
-        elif diff < 90:
-            return locale.describe('minute', sign, only_distance=only_distance)
-        elif diff < 2700:
-            minutes = sign * int(max(delta / 60, 2))
-            return locale.describe('minutes', minutes, only_distance=only_distance)
-
-        elif diff < 5400:
-            return locale.describe('hour', sign, only_distance=only_distance)
-        elif diff < 79200:
-            hours = sign * int(max(delta / 3600, 2))
-            return locale.describe('hours', hours, only_distance=only_distance)
-
-        elif diff < 129600:
-            return locale.describe('day', sign, only_distance=only_distance)
-        elif diff < 2160000:
-            days = sign * int(max(delta / 86400, 2))
-            return locale.describe('days', days, only_distance=only_distance)
-
-        elif diff < 3888000:
-            return locale.describe('month', sign, only_distance=only_distance)
-        elif diff < 29808000:
-            self_months = self._datetime.year * 12 + self._datetime.month
-            other_months = dt.year * 12 + dt.month
-
-            months = sign * int(max(abs(other_months - self_months), 2))
-
-            return locale.describe('months', months, only_distance=only_distance)
-
-        elif diff < 47260800:
-            return locale.describe('year', sign, only_distance=only_distance)
         else:
-            years = sign * int(max(delta / 31536000, 2))
-            return locale.describe('years', years, only_distance=only_distance)
-
-
+            if granularity == 'second':
+                delta = sign * delta
+            elif granularity == 'minute':
+                delta = trunc(sign * delta / 60)
+            elif granularity == 'hour':
+                delta = trunc(sign * delta / (60*60))
+            elif granularity == 'day':
+                delta = trunc(sign * delta / (60*60*24))
+            elif granularity == 'week':
+                delta = trunc(sign * delta / (60*60*24*7))
+            elif granularity == 'month':
+                delta = trunc(sign * delta / (60*60*24*30.5))
+            elif granularity == 'year':
+                delta = trunc(sign * delta / (60*60*24*365.25))
+            else:
+                raise TypeError('Error. Could not understand your level of granularity. Please select between \
+                "second", "minute", "hour", "day", "week", "month" or "year"') 
+            
+            if(delta > 2):
+                granularity += 's'
+            return locale.describe(granularity, delta, only_distance=False)
     # math
 
     def __add__(self, other):
