575952cc3b606c0b0292db55b9617d8ad79a855b
Al Johri <al.johri@gmail.com>
2017-11-08 15:24:53 -0500

    add python 3.6 trove classifier to setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
