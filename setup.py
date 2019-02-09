2e551688111e0d33c9ae723f4ab7a2f9cef15f8d
Chris <30196510+systemcatch@users.noreply.github.com>
2019-02-09 16:57:57 +0000

    Mention Python 3.7 in docs (#564)

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
