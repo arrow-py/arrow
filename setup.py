a84c591ece7a79f1768d1d1f131d9b8a6745cad0
Chris Smith <crsmithdev@gmail.com>
2019-05-18 12:43:10 -0700

    fix grep in setup.py

--- setup.py
+++ setup.py
@@ -14,46 +14,43 @@ def fpath(name):
 
 
 def read(fname):
-    return codecs.open(fpath(fname), encoding='utf-8').read()
+    return codecs.open(fpath(fname), encoding="utf-8").read()
 
 
 def grep(attrname):
-    pattern = r"{}\W*=\W*'([^']+)'".format(attrname)
+    pattern = r'{}\W*=\W*"([^"]+)"'.format(attrname)
+    print(pattern, file_text)
     strval, = re.findall(pattern, file_text)
     return strval
 
 
-file_text = read(fpath('arrow/__init__.py'))
+file_text = read(fpath("arrow/__init__.py"))
 
 setup(
-    name='arrow',
-    version=grep('__version__'),
-    description='Better dates and times for Python',
-    long_description=read(fpath('README.rst')),
-    url='https://github.com/crsmithdev/arrow/',
-    author='Chris Smith',
+    name="arrow",
+    version=grep("__version__"),
+    description="Better dates and times for Python",
+    long_description=read(fpath("README.rst")),
+    url="https://github.com/crsmithdev/arrow/",
+    author="Chris Smith",
     author_email="crsmithdev@gmail.com",
-    license='Apache 2.0',
-    packages=['arrow'],
+    license="Apache 2.0",
+    packages=["arrow"],
     zip_safe=False,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
-    install_requires=[
-        'python-dateutil',
-    ],
-    extras_require={
-        ":python_version=='2.7'": ['backports.functools_lru_cache>=1.2.1'],
-    },
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
+    install_requires=["python-dateutil"],
+    extras_require={":python_version=='2.7'": ["backports.functools_lru_cache>=1.2.1"]},
     test_suite="tests",
-    tests_require=['chai'],
+    tests_require=["chai"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
