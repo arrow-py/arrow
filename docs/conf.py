725f019451f827e7d565d721001ef31d40254b2e
Jad Chaar <jad.chaar@gmail.com>
2019-05-26 12:54:47 -0400

    Exclude README documents in _themes folder

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_themes/README.rst', '_themes/f6/README.rst']
 
 master_doc = 'index'
 source_suffix = '.rst'
