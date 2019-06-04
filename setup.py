95f2be6f556984bf4cacc85bc053db451595ec3e
Andrew Wilson <andrew@andrewmwilson.com>
2019-06-04 12:53:32 +1000

    Always open README and __init__.pyfiles with utf-8 encoding (#601)
    * Always open README with utf-8 encoding
    
    * Always open init.py with utf-8 encoding

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
+import codecs
 import re
 
 from setuptools import setup
 
-with open("README.rst", "r") as f:
+with codecs.open("README.rst", encoding="utf-8") as f:
     readme = f.read()
 
-with open("arrow/__init__.py", "r") as f:
+with codecs.open("arrow/__init__.py", encoding="utf-8") as f:
     init = f.read()
 
 
