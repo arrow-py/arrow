7c701d289bd4f6394145bc9299977919d26802ad
systemcatch <30196510+systemcatch@users.noreply.github.com>
2019-08-04 22:54:10 +0100

    black formatting fixes

--- arrow/locales.py
+++ arrow/locales.py
@@ -1237,33 +1237,68 @@ class RussianLocale(SlavicBaseLocale):
 
 class AfrikaansLocale(Locale):
 
-    names = ['af', 'af_nl']
+    names = ["af", "af_nl"]
 
-    past = '{0} gelede'
-    future = 'in {0}'
+    past = "{0} gelede"
+    future = "in {0}"
 
     timeframes = {
-        'now': 'nou',
-        'seconds': 'sekondes',
-        'minute': 'minuut',
-        'minutes': '{0} minute',
-        'hour': 'uur',
-        'hours': '{0} ure',
-        'day': 'een dag',
-        'days': '{0} dae',
-        'month': 'een maand',
-        'months': '{0} maande',
-        'year': 'een jaar',
-        'years': '{0} jaar',
+        "now": "nou",
+        "seconds": "sekondes",
+        "minute": "minuut",
+        "minutes": "{0} minute",
+        "hour": "uur",
+        "hours": "{0} ure",
+        "day": "een dag",
+        "days": "{0} dae",
+        "month": "een maand",
+        "months": "{0} maande",
+        "year": "een jaar",
+        "years": "{0} jaar",
     }
 
-    month_names = ['', 'Januarie', 'Februarie', 'Maart', 'April', 'Mei', 'Junie', 'Julie',
-        'Augustus', 'September', 'Oktober', 'November', 'Desember']
-    month_abbreviations = ['', 'Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug',
-        'Sep', 'Okt', 'Nov', 'Des']
+    month_names = [
+        "",
+        "Januarie",
+        "Februarie",
+        "Maart",
+        "April",
+        "Mei",
+        "Junie",
+        "Julie",
+        "Augustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember",
+    ]
+    month_abbreviations = [
+        "",
+        "Jan",
+        "Feb",
+        "Mrt",
+        "Apr",
+        "Mei",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Okt",
+        "Nov",
+        "Des",
+    ]
 
-    day_names = ['', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrydag', 'Saterdag', 'Sondag']
-    day_abbreviations = ['', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za', 'So']
+    day_names = [
+        "",
+        "Maandag",
+        "Dinsdag",
+        "Woensdag",
+        "Donderdag",
+        "Vrydag",
+        "Saterdag",
+        "Sondag",
+    ]
+    day_abbreviations = ["", "Ma", "Di", "Wo", "Do", "Vr", "Za", "So"]
 
 
 class BulgarianLocale(SlavicBaseLocale):
