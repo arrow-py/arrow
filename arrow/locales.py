35e33f7a5927047dbda6e04043de9387071ed552
Choco02 <41271075+Choco02@users.noreply.github.com>
2019-08-09 14:46:50 -0300

    Updates to BrazilianPortugueseLocale (#632)
    * BrazilianPortugueseLocale updated

--- arrow/locales.py
+++ arrow/locales.py
@@ -1753,6 +1753,66 @@ class BrazilianPortugueseLocale(PortugueseLocale):
 
     past = "faz {0}"
 
+    future = "em {0}"
+
+    timeframes = {
+        "now": "agora",
+        "seconds": "segundos",
+        "minute": "um minuto",
+        "minutes": "{0} minutos",
+        "hour": "uma hora",
+        "hours": "{0} horas",
+        "day": "um dia",
+        "days": "{0} dias",
+        "month": "um mês",
+        "months": "{0} meses",
+        "year": "um ano",
+        "years": "{0} anos",
+    }
+
+    month_names = [
+        "",
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ]
+    month_abbreviations = [
+        "",
+        "Jan",
+        "Fev",
+        "Mar",
+        "Abr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Set",
+        "Out",
+        "Nov",
+        "Dez",
+    ]
+
+    day_names = [
+        "",
+        "Segunda-feira",
+        "Terça-feira",
+        "Quarta-feira",
+        "Quinta-feira",
+        "Sexta-feira",
+        "Sábado",
+        "Domingo",
+    ]
+    day_abbreviations = ["", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"]
+
 
 class TagalogLocale(Locale):
 
