2d977e1f25f1b0282b6dbdfd83b4e72efa225bfa
Jad Chaar <jad.chaar@gmail.com>
2019-10-01 22:31:28 -0400

    Added comments to constants

--- arrow/constants.py
+++ arrow/constants.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# output of time.mktime(datetime.max.timetuple()) on macOS
-# must hardcode for compatibility with Windows
+# Output of time.mktime(datetime.max.timetuple()) on macOS
+# This value must be hardcoded for compatibility with Windows
+# Platform-independent max timestamps are hard to form
+# https://stackoverflow.com/q/46133223
 MAX_TIMESTAMP = 253402318799.0
 MAX_TIMESTAMP_MS = MAX_TIMESTAMP * 1000
 MAX_TIMESTAMP_US = MAX_TIMESTAMP * 1000000
