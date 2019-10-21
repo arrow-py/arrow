ea8ea9a4d067c9f02cb51fcae5ddfbfe89e02920
systemcatch <30196510+systemcatch@users.noreply.github.com>
2019-10-21 17:51:51 +0100

    Update tz formatter tests

--- tests/formatter_tests.py
+++ tests/formatter_tests.py
@@ -122,7 +122,7 @@ class DateTimeFormatterFormatTokenTests(Chai):
     def test_timezone_formatter(self):
 
         tz_map = {
-            # 'BRST': 'America/Sao_Paulo', TODO investigate why this fails
+            "CST": "Asia/Shanghai",
             "CET": "Europe/Berlin",
             "JST": "Asia/Tokyo",
             "PST": "US/Pacific",
