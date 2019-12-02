dbef6fcf1fad902a52bdcaaceab883b01344c400
ZhangQingsen <1463769805zqs@gmail.com>
2019-12-02 03:14:55 -0500

    add tests

--- arrow/my test.py
+++ arrow/my test.py
@@ -1,3 +1,38 @@
 # -*- coding: utf-8 -*-
 import arrow
-arrow.Arrow(2013, 5, 5, 12, 30, 45)
+utc = arrow.utcnow()
+class ArrowDehumanizeTestsWithLocale(Chai):
+    def setUp(self):
+        super(ArrowDehumanizeTestsWithLocale, self).setUp()
+
+        self.datetime = datetime(2013, 1, 1)
+
+    def test_now(self):
+
+        arw = arrow.Arrow(2013, 1, 1, 0, 0, 0)
+
+        result = arw.dehumanize()
+
+        self.assertEqual(result, arw)
+
+    def test_days(self):
+        arw = arrow.Arrow(2013, 1, 2, 0, 0, 0)
+        arw_1 = arrow.Arrow(2013, 1, 3, 0, 0, 0)
+        arw_0 = arrow.Arrow(2013, 1, 1, 0, 0, 0)
+
+        result = arw_1.dehumanize('1 days ago')
+        result_1 = arw_0.dehumanize('in 1 days')
+
+        self.assertEqual(result, arw_0)
+        self.assertEqual(result_1, arw)
+
+    def test_years(self):
+        arw = arrow.Arrow(2014, 1, 1, 0, 0, 0)
+        arw_1 = arrow.Arrow(2015, 1, 1, 0, 0, 0)
+        arw_0 = arrow.Arrow(2013, 1, 1, 0, 0, 0)
+
+        result = arw_1.dehumanize('1 years ago')
+        result_1 = arw_0.dehumanize('in 1 years')
+
+        self.assertEqual(result, arw_0)
+        self.assertEqual(result_1, arw)
