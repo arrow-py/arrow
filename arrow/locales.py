a75f3bba71b315f4e9b9e48403791a961c7e5318
Sunny <sunnzy@gmail.com>
2019-12-27 23:49:09 +1100

    Added Hong Kong (zh_hk) locale

--- arrow/locales.py
+++ arrow/locales.py
@@ -932,6 +932,66 @@ class ChineseTWLocale(Locale):
     day_abbreviations = ["", "一", "二", "三", "四", "五", "六", "日"]
 
 
+class HongKongLocale(Locale):
+
+    names = ["zh_hk"]
+
+    past = "{0}前"
+    future = "{0}後"
+
+    timeframes = {
+        "now": "剛才",
+        "second": "1秒",
+        "seconds": "{0}秒",
+        "minute": "1分鐘",
+        "minutes": "{0}分鐘",
+        "hour": "1小時",
+        "hours": "{0}小時",
+        "day": "1天",
+        "days": "{0}天",
+        "week": "1星期",
+        "weeks": "{0}星期",
+        "month": "1個月",
+        "months": "{0}個月",
+        "year": "1年",
+        "years": "{0}年",
+    }
+
+    month_names = [
+        "",
+        "1月",
+        "2月",
+        "3月",
+        "4月",
+        "5月",
+        "6月",
+        "7月",
+        "8月",
+        "9月",
+        "10月",
+        "11月",
+        "12月",
+    ]
+    month_abbreviations = [
+        "",
+        " 1",
+        " 2",
+        " 3",
+        " 4",
+        " 5",
+        " 6",
+        " 7",
+        " 8",
+        " 9",
+        "10",
+        "11",
+        "12",
+    ]
+
+    day_names = ["", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    day_abbreviations = ["", "一", "二", "三", "四", "五", "六", "日"]
+
+
 class KoreanLocale(Locale):
 
     names = ["ko", "ko_kr"]
