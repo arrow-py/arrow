c017ee69e4bdf13993046fd090651cec8627ffbd
systemcatch <30196510+systemcatch@users.noreply.github.com>
2020-05-20 17:29:48 +0100

    One comment always escapes

--- arrow/parser.py
+++ arrow/parser.py
@@ -88,14 +88,6 @@ class DateTimeParser(object):
 
     SEPARATORS = ["-", "/", "."]
 
-    # # Output of time.mktime(datetime.max.timetuple()) on macOS
-    # # This value must be hardcoded for compatibility with Windows
-    # # Platform-independent max timestamps are hard to form
-    # # https://stackoverflow.com/q/46133223
-    # MAX_TIMESTAMP = 253402318799.0
-    # MAX_TIMESTAMP_MS = MAX_TIMESTAMP * 1e3
-    # MAX_TIMESTAMP_US = MAX_TIMESTAMP * 1e6
-
     def __init__(self, locale="en_us", cache_size=0):
 
         self.locale = locales.get_locale(locale)
