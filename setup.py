6555b12273f225f61d43ff0927a6618b0d082157
Jad Chaar <jad.chaar@gmail.com>
2020-06-06 14:00:23 -0400

    Added classifiers to setup.py and add Python 3.9 reference in docs

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="arrow date time datetime timestamp timezone",
     project_urls={
