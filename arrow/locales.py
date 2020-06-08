6e17e229a90aed28732c6526eb93e94853a41029
Antony Oduor <antouboduor@gmail.com>
2020-06-08 08:24:49 -0600

    Add Swahili Locale (#797)
    * Add Swahili Locale
    
    Covers sw_ke and sw_tz
    
    Signed-off-by: Antony Oduor <aowino@gmail.com>
    
    * Add Tests for Swahili Locale
    
    * Update Timeframes for Swahili Locale
    
    * Update Linting Corrections :)
    
    Signed-off-by: Antony Oduor <aowino@gmail.com>
    
    * Correct 'Past' and Week Timeframes; Update Tests
    
    Signed-off-by: Antony Oduor <aowino@gmail.com>
    
    * Additional Tests for Swahili Locale
    
    These are for past and future humanize strings
    
    Signed-off-by: Antony Oduor <aowino@gmail.com>
    
    * Fix Typo on Month Abbreviations
    
    Signed-off-by: Antony Oduor <aowino@gmail.com>

--- arrow/locales.py
+++ arrow/locales.py
@@ -4139,4 +4139,89 @@ class EstonianLocale(Locale):
         return form.format(abs(delta))
 
 
+class SwahiliLocale(Locale):
+
+    names = [
+        "sw",
+        "sw_ke",
+        "sw_tz",
+    ]
+
+    past = "{0} iliyopita"
+    future = "muda wa {0}"
+    and_word = "na"
+
+    timeframes = {
+        "now": "sasa hivi",
+        "second": "sekunde",
+        "seconds": "sekunde {0}",
+        "minute": "dakika moja",
+        "minutes": "dakika {0}",
+        "hour": "saa moja",
+        "hours": "saa {0}",
+        "day": "siku moja",
+        "days": "siku {0}",
+        "week": "wiki moja",
+        "weeks": "wiki {0}",
+        "month": "mwezi moja",
+        "months": "miezi {0}",
+        "year": "mwaka moja",
+        "years": "miaka {0}",
+    }
+
+    meridians = {"am": "asu", "pm": "mch", "AM": "ASU", "PM": "MCH"}
+
+    month_names = [
+        "",
+        "Januari",
+        "Februari",
+        "Machi",
+        "Aprili",
+        "Mei",
+        "Juni",
+        "Julai",
+        "Agosti",
+        "Septemba",
+        "Oktoba",
+        "Novemba",
+        "Desemba",
+    ]
+    month_abbreviations = [
+        "",
+        "Jan",
+        "Feb",
+        "Mac",
+        "Apr",
+        "Mei",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Sep",
+        "Okt",
+        "Nov",
+        "Des",
+    ]
+
+    day_names = [
+        "",
+        "Jumatatu",
+        "Jumanne",
+        "Jumatano",
+        "Alhamisi",
+        "Ijumaa",
+        "Jumamosi",
+        "Jumapili",
+    ]
+    day_abbreviations = [
+        "",
+        "Jumatatu",
+        "Jumanne",
+        "Jumatano",
+        "Alhamisi",
+        "Ijumaa",
+        "Jumamosi",
+        "Jumapili",
+    ]
+
+
 _locales = _map_locales()
