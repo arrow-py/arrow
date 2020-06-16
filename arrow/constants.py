277d74ba6f098743ad745faaac4d93b83048535f
Kristijan "Fremen" Velkovski <me@krisfremen.com>
2020-06-16 17:13:17 +0000

    Replace hardcoded MAX_TIMESTAMP constant with datetime.max.timestamp()

--- arrow/constants.py
+++ arrow/constants.py
@@ -2,6 +2,8 @@
 # This value must be hardcoded for compatibility with Windows
 # Platform-independent max timestamps are hard to form
 # https://stackoverflow.com/q/46133223
-MAX_TIMESTAMP = 253402318799.0
+from datetime import datetime
+
+MAX_TIMESTAMP = datetime.max.timestamp()
 MAX_TIMESTAMP_MS = MAX_TIMESTAMP * 1000
 MAX_TIMESTAMP_US = MAX_TIMESTAMP * 1000000
