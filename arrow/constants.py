edc2e1730d83b66add598c451b1e9ccc2131c7c1
Kristijan "Fremen" Velkovski <me@krisfremen.com>
2020-09-16 02:47:57 -0400

    Move windows based timestamp to a more sane standard datetime.datetime instead of

--- arrow/constants.py
+++ arrow/constants.py
@@ -2,6 +2,12 @@
 # This value must be hardcoded for compatibility with Windows
 # Platform-independent max timestamps are hard to form
 # https://stackoverflow.com/q/46133223
-MAX_TIMESTAMP = 253402318799.0
+import os
+from datetime import datetime
+
+if os.name == "nt":  # type: ignore
+    MAX_TIMESTAMP = datetime(3001, 1, 18, 23, 59, 59, 999999).timestamp()
+else:
+    MAX_TIMESTAMP = datetime.max.timestamp()
 MAX_TIMESTAMP_MS = MAX_TIMESTAMP * 1000
 MAX_TIMESTAMP_US = MAX_TIMESTAMP * 1000000
