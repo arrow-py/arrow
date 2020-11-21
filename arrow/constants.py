397f5f2e2ade50f2c3c2739146fa3488421a3239
Byeonghoon Yoo <bh322yoo@gmail.com>
2020-11-22 01:27:24 +0900

    Apply Final to constants.py

--- arrow/constants.py
+++ arrow/constants.py
@@ -1,6 +1,11 @@
 import sys
 from datetime import datetime
 
+if sys.version_info < (3, 8):
+    from typing_extensions import Final
+else:
+    from typing import Final
+
 # datetime.max.timestamp() errors on Windows, so we must hardcode
 # the highest possible datetime value that can output a timestamp.
 # tl;dr platform-independent max timestamps are hard to form
@@ -8,17 +13,18 @@ from datetime import datetime
 try:
     # Get max timestamp. Works on POSIX-based systems like Linux and macOS,
     # but will trigger an OverflowError, ValueError, or OSError on Windows
-    MAX_TIMESTAMP = datetime.max.timestamp()
+    _MAX_TIMESTAMP = datetime.max.timestamp()
 except (OverflowError, ValueError, OSError):  # pragma: no cover
     # Fallback for Windows if initial max timestamp call fails
     # Must get max value of ctime on Windows based on architecture (x32 vs x64)
     # https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/ctime-ctime32-ctime64-wctime-wctime32-wctime64
     is_64bits = sys.maxsize > 2 ** 32
-    MAX_TIMESTAMP = (
+    _MAX_TIMESTAMP = (
         datetime(3000, 12, 31, 23, 59, 59, 999999).timestamp()
         if is_64bits
         else datetime(2038, 1, 18, 23, 59, 59, 999999).timestamp()
     )
 
-MAX_TIMESTAMP_MS = MAX_TIMESTAMP * 1e3
-MAX_TIMESTAMP_US = MAX_TIMESTAMP * 1e6
+MAX_TIMESTAMP: Final[float] = _MAX_TIMESTAMP
+MAX_TIMESTAMP_MS: Final[float] = MAX_TIMESTAMP * 1e3
+MAX_TIMESTAMP_US: Final[float] = MAX_TIMESTAMP * 1e6
