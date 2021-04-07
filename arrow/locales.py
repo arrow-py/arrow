85964d0d4eb3a957826fc60c6f992984e4411f2f
cyriaka90 <jogoheininger@aol.com>
2021-04-08 01:09:43 +0200

    Add Lithuanian (#950)

--- arrow/locales.py
+++ arrow/locales.py
@@ -4484,4 +4484,85 @@ class CroatianLocale(Locale):
         return form.format(delta)
 
 
+class LithuanianLocale(Locale):
+
+    names = ["lt", "lt-lt"]
+
+    past = "prieš {0}"
+    future = "po {0}"
+    and_word = "ir"
+
+    timeframes: ClassVar[Mapping[TimeFrameLiteral, Union[str, Mapping[str, str]]]] = {
+        "now": "dabar",
+        "second": "sekundės",
+        "seconds": "{0} sekundžių",
+        "minute": "minutės",
+        "minutes": "{0} minučių",
+        "hour": "valandos",
+        "hours": "{0} valandų",
+        "day": "dieną",
+        "days": "{0} dienų",
+        "week": "savaitės",
+        "weeks": "{0} savaičių",
+        "month": "mėnesio",
+        "months": "{0} mėnesių",
+        "year": "metų",
+        "years": "{0} metų",
+    }
+
+    month_names = [
+        "",
+        "sausis",
+        "vasaris",
+        "kovas",
+        "balandis",
+        "gegužė",
+        "birželis",
+        "liepa",
+        "rugpjūtis",
+        "rugsėjis",
+        "spalis",
+        "lapkritis",
+        "gruodis",
+    ]
+
+    month_abbreviations = [
+        "",
+        "saus",
+        "vas",
+        "kovas",
+        "bal",
+        "geg",
+        "birž",
+        "liepa",
+        "rugp",
+        "rugs",
+        "spalis",
+        "lapkr",
+        "gr",
+    ]
+
+    day_names = [
+        "",
+        "pirmadienis",
+        "antradienis",
+        "trečiadienis",
+        "ketvirtadienis",
+        "penktadienis",
+        "šeštadienis",
+        "sekmadienis",
+    ]
+
+    day_abbreviations = [
+        "",
+        "pi",
+        "an",
+        "tr",
+        "ke",
+        "pe",
+        "še",
+        "se",
+    ]
+
+
 _locales: Dict[str, Type[Locale]] = _map_locales()
