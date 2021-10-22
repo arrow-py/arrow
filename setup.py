9652cd960db7ecbe9282185caee5f733a4a2f4d7
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-22 18:56:49 +0300

    Declare support for Python 3.10

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="arrow date time datetime timestamp timezone humanize",
     project_urls={
